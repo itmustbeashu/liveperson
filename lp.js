ace845785ce62b2d06bc68c75b719221c4545d60
Ashish Singh <78902772+itmustbeashu@users.noreply.github.com>
2022-05-24 23:08:09 +0530

    Update lp.js

--- lp.js
+++ lp.js
@@ -1,36 +1,35 @@
+
+lpTag.sdes = lpTag.sdes || [];
 lpTag.identities = [];
-        
-        
 function identityFn(callback) {
     callback({
         iss: "Ashu",
         acr: "loa1",
-        sub: "16723672"
+        sub: "4255551212"
     });
-} 
+}
 
-lpTag.identities.push(identityFn);
-  
-lpTag.sdes = lpTag.sdes||[];
- var data = [{
+
+lpTag.sdes.push({
     "type": "cart",
-    "total": 1099, 
-    "currency": "USD", 
-    "numItems": 1, 
+    "total": 1099,
+    "currency": "USD",
+    "numItems": "1",
     "products": [{
-      "product": {
-      "name": "Shop Online", 
-      "category": " ", 
-      "price": " " 
-      }
-}]
-}]
+        "product": {
+            "name": "Title : Shop Online",
+            "category": " ",
+            "price": " "
+        }
+    }]
+})
+lpTag.identities.push(identityFn);
 
-lpTag.sdes.push(data);
 
-function getAuthToken(callback) {
-    console.log('Authentication token requested by Liveperson 2');
-    let token = localStorage.getItem('token')
-    callback(token);            
-  }
-  
+
+
+
+
+
+
+
